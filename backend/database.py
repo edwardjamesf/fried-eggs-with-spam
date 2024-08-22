b59e0f0b63a93f8c6a97207690aff04ead66484e
EJ Fernandez <edwardjamesf@gmail.com>
2024-08-22 10:10:00 -0700

    Fix bugs in backend

--- backend/database.py
+++ backend/database.py
@@ -5,9 +5,8 @@ import os
 load_dotenv()
 db_user = os.getenv("POSTGRES_USER")
 db_password = os.getenv("POSTGRES_PASSWORD")
-# db_ip_port = os.getenv("POSTGRES_IP_PORT")
+db_ip_port = os.getenv("POSTGRES_IP_PORT")
 db_name = os.getenv("POSTGRES_DB")
-# postgres_url = f"postgresql://{db_user}:{db_password}@{db_ip_port}/{db_name}"
-postgres_url = f"postgresql+psycopg2://{db_user}:{db_password}@pgdb:5432/{db_name}"
+postgres_url = f"postgresql://{db_user}:{db_password}@{db_ip_port}/{db_name}"
 print("postgres_url: " + postgres_url)
 engine = create_engine(postgres_url, echo=True)
