37bb96274f63334f5b0f5e97053bbdd71c62b99f
EJ Fernandez <edwardjamesf@gmail.com>
2024-08-29 14:34:42 -0700

    Fix bug frontend 3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
+    host: '0.0.0.0',
     port: 4000,
     proxy: {
       "/api": {
