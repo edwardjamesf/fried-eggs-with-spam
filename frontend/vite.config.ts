38499e9ad8481f4134aef4afcf5e8e27d0d01396
EJ Fernandez <edwardjamesf@gmail.com>
2024-09-22 13:10:41 -0700

    Fix bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +12,7 @@ export default defineConfig({
     port: 4000,
     proxy: {
       "/api": {
-        target: "http://localhost:8000", // "http://192.168.86.241:8000",
+        target: 'http://192.168.86.241:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/,""),
         secure: false,
