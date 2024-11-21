fd9cd7e943552bad47db9f07b5ce072f7e05fb2d
EJ Fernandez <edwardjamesf@gmail.com>
2024-11-20 18:41:16 -0800

    Convert double types into BigDecimal types

--- backend/src/main/java/org/fews/backend/model/PurchaseDto.java
+++ backend/src/main/java/org/fews/backend/model/PurchaseDto.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Schema(name = "PurchaseDto", description = "Input required for adding purchases to database")
@@ -19,19 +20,19 @@ public class PurchaseDto {
     @Schema(description = "Date of purchase, in YYYY-MM-DD format", example = "2021-01-19")
     private String purchaseDate;
     @Schema(description = "Base cost of purchase", example = "133.99")
-    private double costBase;
+    private BigDecimal costBase;
     @Schema(description = "Tax paid on purchase", example = "10.38")
-    private double costTax;
+    private BigDecimal costTax;
     @Schema(description = "Shipping costs of purchase", example = "0.0")
-    private double costShipping;
+    private BigDecimal costShipping;
     @Schema(description = "Other fees paid pertaining to the purchase", example = "0.0")
-    private double costOther;
+    private BigDecimal costOther;
     @Schema(description = "Notes about the purchase", example = "Grey Japanese model 1. Includes power and composite A/V cables, 1 controller, and Vampire Hunter (JP) game")
     private String notes;
-    @Schema(description = "ID of the associated image, if applicable", example = "24695b27-8093-46d3-b51e-f83687093342")
+    @Schema(description = "ID of the associated image, if applicable", example = "00000000-0000-0000-0000-000000000000")
     private UUID imageId;
-    @Schema(description = "ID of the associated console, if applicable", example = "24695b27-8093-46d3-b51e-f83687093342")
+    @Schema(description = "ID of the associated console, if applicable", example = "00000000-0000-0000-0000-000000000000")
     private UUID consoleId;
-    @Schema(description = "ID of the associated game, if applicable", example = "24695b27-8093-46d3-b51e-f83687093342")
+    @Schema(description = "ID of the associated game, if applicable", example = "00000000-0000-0000-0000-000000000000")
     private UUID gameId;
 }
